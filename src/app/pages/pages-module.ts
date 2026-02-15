ddfc42a920283ae265e1587830ed0dcc37354db5
Competitivecodelearner <akulasatish49@gmail.com>
2026-02-15 16:04:20 +0530

    about section added

--- src/app/pages/pages-module.ts
+++ src/app/pages/pages-module.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero/hero';
 import { Highlights } from './highlights/highlights';
 import { Populardest } from './populardest/populardest';
 import { Router, RouterModule } from '@angular/router';
+import { About } from './about/about';
 
 
 
@@ -22,6 +23,7 @@ import { Router, RouterModule } from '@angular/router';
     Hero,
     Highlights,
     Populardest,
+    About,
   
   
   ],
