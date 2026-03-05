9c4a2ba7ae48e63edc6c587419ef65ec807eeba1
Competitivecodelearner <akulasatish49@gmail.com>
2026-03-05 20:57:36 +0530

    first 2 features of analytics section added

--- src/app/pages/analytics/analytics.ts
+++ src/app/pages/analytics/analytics.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Bookings, TestBookingData } from '../../services/booking-data';
+import {Chart} from 'chart.js/auto';
+import { ViewChild,ElementRef } from '@angular/core';
+import { submit } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-analytics',
@@ -6,6 +10,91 @@ import { Component } from '@angular/core';
   templateUrl: './analytics.html',
   styleUrl: './analytics.css',
 })
-export class Analytics {
+export class Analytics implements AfterViewInit{
+  bookings:Bookings[]=[];
+  destinationcounts:{[key:string]:number}={};
+  BookingChart!:Chart;
+  @ViewChild('popularityPieChart') popularityChartRef!:ElementRef;
+  popularityChart!:Chart;
+  topDestinations:{destination:string,count:number}[]=[];
+  leastDestinations:{destination:string,count:number}[]=[];
+  constructor(private bookingservice:TestBookingData){}
 
+  ngAfterViewInit(){
+    this.loadBookings();
+  }
+
+  loadBookings(){
+    this.bookingservice.getBookings().subscribe( data=>{
+      this.bookings=data;
+      this.calculateBookingDistribution();
+      this.createBarChart();
+      this.createPopularityChart();
+    })
+  }
+
+  calculateBookingDistribution(){
+    this.destinationcounts={};
+    this.bookings.forEach(b=>{
+      if(this.destinationcounts[b.destination]){
+        this.destinationcounts[b.destination]++;
+      }
+      else{
+        this.destinationcounts[b.destination]=1;
+      }
+    });
+  }
+
+  createBarChart(){
+    const destinations=Object.keys(this.destinationcounts);
+    const counts=Object.values(this.destinationcounts);
+    if(this.BookingChart){
+      this.BookingChart.destroy();
+    }
+    this.BookingChart=new Chart("bookingBarChart",{
+      type:'bar',
+      data:{
+        labels:destinations,
+        datasets:[
+          {
+            label:'Bookings',
+            data:counts,
+            backgroundColor:'#089cce'
+          }
+        ]
+      },
+      options:{
+        responsive:true,
+        maintainAspectRatio:false
+      }
+    });
+  }
+  createPopularityChart(){
+    const destinations=Object.keys(this.destinationcounts);
+    const counts=Object.values(this.destinationcounts);
+    const total=counts.reduce((sum,val)=>sum+val,0);
+    const percentages=counts.map(c=>Number(((c/total)*100).toFixed(1)));
+    this.popularityChart = new Chart(this.popularityChartRef.nativeElement,{
+      type:'pie',
+      data:{
+        labels:destinations,
+        datasets:[
+          {
+            data:percentages,
+            backgroundColor:[
+              '089cce',
+              '#1fc5db',
+            '#4caf50',
+            '#ff9800',
+            '#9c27b0'
+            ]
+          }
+        ]
+      },
+      options:{
+        responsive:true,
+        maintainAspectRatio:false
+      }
+    });
+  }
 }
